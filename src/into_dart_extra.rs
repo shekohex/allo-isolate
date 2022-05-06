9b6abea121b22af72e9994efc35902fb4d54508a
Daniel Hahne <d@niel.re>
2022-05-06 19:18:15 +0200

    Correct IntoDart usize

--- src/into_dart_extra.rs
+++ src/into_dart_extra.rs
@@ -5,39 +5,63 @@
 use crate::{ffi::*, IntoDart};
 
 impl IntoDart for i8 {
-    fn into_dart(self) -> DartCObject { (self as i32).into_dart() }
+    fn into_dart(self) -> DartCObject {
+        (self as i32).into_dart()
+    }
 }
 
 impl IntoDart for i16 {
-    fn into_dart(self) -> DartCObject { (self as i32).into_dart() }
+    fn into_dart(self) -> DartCObject {
+        (self as i32).into_dart()
+    }
 }
 
 impl IntoDart for u8 {
-    fn into_dart(self) -> DartCObject { (self as i32).into_dart() }
+    fn into_dart(self) -> DartCObject {
+        (self as i32).into_dart()
+    }
 }
 
 impl IntoDart for u16 {
-    fn into_dart(self) -> DartCObject { (self as i32).into_dart() }
+    fn into_dart(self) -> DartCObject {
+        (self as i32).into_dart()
+    }
 }
 
 impl IntoDart for u32 {
-    fn into_dart(self) -> DartCObject { (self as i64).into_dart() }
+    fn into_dart(self) -> DartCObject {
+        (self as i64).into_dart()
+    }
 }
 
 impl IntoDart for u64 {
-    fn into_dart(self) -> DartCObject { (self as i64).into_dart() }
+    fn into_dart(self) -> DartCObject {
+        (self as i64).into_dart()
+    }
 }
 
 impl IntoDart for i128 {
-    fn into_dart(self) -> DartCObject { self.to_string().into_dart() }
+    fn into_dart(self) -> DartCObject {
+        self.to_string().into_dart()
+    }
 }
 
 impl IntoDart for u128 {
-    fn into_dart(self) -> DartCObject { self.to_string().into_dart() }
+    fn into_dart(self) -> DartCObject {
+        self.to_string().into_dart()
+    }
 }
 
 #[cfg(target_pointer_width = "64")]
+impl IntoDart for usize {
+    fn into_dart(self) -> DartCObject {
+        (self as u64 as i64).into_dart()
+    }
+}
+
 #[cfg(target_pointer_width = "32")]
 impl IntoDart for usize {
-    fn into_dart(self) -> DartCObject { (self as u32 as i32).into_dart() }
+    fn into_dart(self) -> DartCObject {
+        (self as u32 as i32).into_dart()
+    }
 }
