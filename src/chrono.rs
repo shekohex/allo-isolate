f4adaf7be936ecd45a9ee28c5d523740ffa1022f
Roms1383 <romain.kelifa@gmail.com>
2022-09-09 18:01:19 +0700

    :construction: impl trait for generic date time, and naive date time

--- src/chrono.rs
+++ src/chrono.rs
@@ -1,5 +1,12 @@
 use crate::{ffi::DartCObject, IntoDart};
 
-impl IntoDart for chrono::DateTime<chrono::Utc> {
+impl<T> IntoDart for chrono::DateTime<T>
+where
+    T: chrono::TimeZone,
+{
+    fn into_dart(self) -> DartCObject { self.timestamp_micros().into_dart() }
+}
+
+impl IntoDart for chrono::NaiveDateTime {
     fn into_dart(self) -> DartCObject { self.timestamp_micros().into_dart() }
 }
