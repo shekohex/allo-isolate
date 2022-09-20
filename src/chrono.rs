b70c1ed458ade01935c601f332a7e1c49dbc94ce
Rom's <romain.kelifa@gmail.com>
2022-09-20 21:03:57 +0700

    Fix/doc chrono (#29)

--- src/chrono.rs
+++ src/chrono.rs
@@ -1,6 +1,10 @@
 //! chrono types
 //!
 //! based on Dart VM, microseconds unit is used
+//!
+//! recommendations below implies UTC based conversions,
+//! as these are generally easier to work with.
+//! > see [timestamp_micros](https://docs.rs/chrono/0.4.20/chrono/naive/struct.NaiveDateTime.html?search=timestamp_micros#method.timestamp_micros)
 
 use crate::{ffi::DartCObject, IntoDart};
 
@@ -14,11 +18,14 @@ impl IntoDart for chrono::DateTime<chrono::Utc> {
     ///   ```rust,ignore
     ///   let s = (raw / 1_000_000) as i64;
     ///   let ns = (raw.rem_euclid(1_000_000) * 1_000) as u32;
-    ///   chrono::DateTime::<chrono::Utc>::from_utc(chrono::NaiveDateTime::from_timestamp(s, ns), chrono::Utc);
+    ///   chrono::DateTime::<chrono::Utc>::from_utc(
+    ///     chrono::NaiveDateTime::from_timestamp(s, ns), chrono::Utc);
     ///   ```
     ///
-    ///   note that it could overflow under the same conditions as of [chrono::NaiveDateTime::from_timestamp](https://docs.rs/chrono/0.4.22/chrono/naive/struct.NaiveDateTime.html#method.from_timestamp)
-    fn into_dart(self) -> DartCObject { self.timestamp_micros().into_dart() }
+    ///   note that it could overflow under the same conditions as of [chrono::NaiveDateTime::from_timestamp](https://docs.rs/chrono/0.4.20/chrono/naive/struct.NaiveDateTime.html#method.from_timestamp)
+    fn into_dart(self) -> DartCObject {
+        self.timestamp_micros().into_dart()
+    }
 }
 
 impl IntoDart for chrono::DateTime<chrono::Local> {
@@ -31,17 +38,34 @@ impl IntoDart for chrono::DateTime<chrono::Local> {
     ///   ```rust,ignore
     ///   let s = (raw / 1_000_000) as i64;
     ///   let ns = (raw.rem_euclid(1_000_000) * 1_000) as u32;
-    ///   chrono::DateTime::<chrono::Local>::from_local(chrono::NaiveDateTime::from_timestamp(s, ns), chrono::Local);
+    ///   chrono::DateTime::<chrono::Local>::from(
+    ///     chrono::DateTime::<chrono::Utc>::from_utc(
+    ///       chrono::NaiveDateTime::from_timestamp(s, ns), chrono::Utc));
     ///   ```
     ///
-    ///   note that it could overflow under the same conditions as of [chrono::NaiveDateTime::from_timestamp](https://docs.rs/chrono/0.4.22/chrono/naive/struct.NaiveDateTime.html#method.from_timestamp)
-    fn into_dart(self) -> DartCObject { self.timestamp_micros().into_dart() }
+    ///   note that it could overflow under the same conditions as of [chrono::NaiveDateTime::from_timestamp](https://docs.rs/chrono/0.4.20/chrono/naive/struct.NaiveDateTime.html#method.from_timestamp)
+    fn into_dart(self) -> DartCObject {
+        self.timestamp_micros().into_dart()
+    }
 }
 
 impl IntoDart for chrono::NaiveDateTime {
-    /// on the other side of FFI, value should be reconstructed like
-    /// [DateTime](chrono::DateTime)::<[Local](chrono::Local)>
-    fn into_dart(self) -> DartCObject { self.timestamp_micros().into_dart() }
+    /// on the other side of FFI, value should be reconstructed like:
+    ///
+    /// - hydrate into Dart [DateTime](https://api.dart.dev/stable/2.18.0/dart-core/DateTime/DateTime.fromMicrosecondsSinceEpoch.html)
+    ///   `DateTime.fromMicrosecondsSinceEpoch(raw, isUtc: true);`
+    ///
+    /// - hydrate into Rust [NaiveDateTime](chrono::NaiveDateTime)
+    ///   ```rust,ignore
+    ///   let s = (raw / 1_000_000) as i64;
+    ///   let ns = (raw.rem_euclid(1_000_000) * 1_000) as u32;
+    ///   chrono::NaiveDateTime::from_timestamp(s, ns)
+    ///   ```
+    ///
+    ///   note that it could overflow under the same conditions as of [chrono::NaiveDateTime::from_timestamp](https://docs.rs/chrono/0.4.20/chrono/naive/struct.NaiveDateTime.html#method.from_timestamp)
+    fn into_dart(self) -> DartCObject {
+        self.timestamp_micros().into_dart()
+    }
 }
 
 impl IntoDart for chrono::Duration {
@@ -52,5 +76,7 @@ impl IntoDart for chrono::Duration {
     ///
     /// - hydrate into Rust [Duration](chrono::Duration)
     /// `chrono::Duration::microseconds(raw);`
-    fn into_dart(self) -> DartCObject { self.num_microseconds().into_dart() }
+    fn into_dart(self) -> DartCObject {
+        self.num_microseconds().into_dart()
+    }
 }
