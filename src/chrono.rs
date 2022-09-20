21d57034e23bc2cec63a733d87e31da08def1e7b
Roms1383 <romain.kelifa@gmail.com>
2022-09-20 17:00:27 +0700

    :memo: add conversion recommendation for naive datetime

--- src/chrono.rs
+++ src/chrono.rs
@@ -46,8 +46,19 @@ impl IntoDart for chrono::DateTime<chrono::Local> {
 }
 
 impl IntoDart for chrono::NaiveDateTime {
-    /// on the other side of FFI, value should be reconstructed like
-    /// [DateTime](chrono::DateTime)::<[Local](chrono::Local)>
+    /// on the other side of FFI, value should be reconstructed like:
+    ///
+    /// - hydrate into Dart [DateTime](https://api.dart.dev/stable/2.18.0/dart-core/DateTime/DateTime.fromMicrosecondsSinceEpoch.html)
+    ///   `DateTime.fromMicrosecondsSinceEpoch(raw, isUtc: true);`
+    ///
+    /// - hydrate into Rust [NaiveDateTime](chrono::NaiveDateTime)
+    ///   ```rust,ignore
+    ///   let s = (raw / 1_000_000) as i64;
+    ///   let ns = (raw.rem_euclid(1_000_000) * 1_000) as u32;
+    ///   chrono::NaiveDateTime::from_timestamp(s, ns)
+    ///   ```
+    ///
+    ///   note that it could overflow under the same conditions as of [chrono::NaiveDateTime::from_timestamp](https://docs.rs/chrono/0.4.20/chrono/naive/struct.NaiveDateTime.html#method.from_timestamp)
     fn into_dart(self) -> DartCObject { self.timestamp_micros().into_dart() }
 }
 
