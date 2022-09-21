4362423a6c4b26cb7f3409d5142232e4bb07ddbf
Roms1383 <romain.kelifa@gmail.com>
2022-09-21 08:47:08 +0700

    :recycle: delegate to Vec<u8>
    Vec<u8> can be converted to Uint8List instead of List<T>, which is notoriously better for performance

--- src/uuid.rs
+++ src/uuid.rs
@@ -3,8 +3,8 @@
 use crate::IntoDart;
 
 impl IntoDart for uuid::Uuid {
-    /// delegate to `[u8;16]` implementation
+    /// delegate to `Vec<u8>` implementation
     fn into_dart(self) -> crate::ffi::DartCObject {
-        (*self.as_bytes()).into_dart()
+        self.as_bytes().to_vec().into_dart()
     }
 }
