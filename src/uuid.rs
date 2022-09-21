d2f9035bc5491346db7eb3f0fe198f39ca483de8
Roms1383 <romain.kelifa@gmail.com>
2022-09-21 09:48:59 +0700

    :memo: add conversion recommendation

--- src/uuid.rs
+++ src/uuid.rs
@@ -4,6 +4,16 @@ use crate::{ffi::DartCObject, IntoDart};
 
 impl IntoDart for uuid::Uuid {
     /// delegate to `Vec<u8>` implementation
+    /// 
+    /// on the other side of FFI, value should be reconstructed like:
+    ///
+    /// - hydrate into Dart [UuidValue](https://pub.dev/documentation/uuid/latest/uuid/UuidValue-class.html)
+    ///   `UuidValue.fromByteList(raw);`
+    ///
+    /// - hydrate into Rust [Uuid](uuid::Uuid)
+    ///   ```rust,ignore
+    ///   uuid::Uuid::from_bytes(*<&[u8] as std::convert::TryInto<&[u8;16]>>::try_into(raw.as_slice()).expect("invalid uuid slice"));
+    ///   ```
     fn into_dart(self) -> DartCObject {
         self.as_bytes().to_vec().into_dart()
     }
