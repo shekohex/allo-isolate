a860c1bf52a90819807c4f4a0606cac23b3ed684
Roms1383 <romain.kelifa@gmail.com>
2022-09-21 11:22:04 +0700

    :memo: add conversion recommendation

--- src/uuid.rs
+++ src/uuid.rs
@@ -21,6 +21,26 @@ impl IntoDart for uuid::Uuid {
 
 impl IntoDart for Vec<uuid::Uuid> {
     /// delegate to `Vec<u8>` implementation but concatenates outputs in a single Vec<u8>
+    ///
+    /// on the other side of FFI, value should be reconstructed like:
+    ///
+    /// - hydrate into Dart [UuidValue](https://pub.dev/documentation/uuid/latest/uuid/UuidValue-class.html)
+    ///   ```dart
+    ///   final count = raw.lengthInBytes / 16;
+    ///   var List<UuidValue> ids = List(growable: true);
+    ///   for (var i = 0; i < count; i += 16) {
+    ///     ids.add(UuidValue.fromByteList(Uint8List.view(raw.buffer, i * 16, 16)))
+    ///   }
+    ///   ```
+    ///
+    /// - hydrate into Rust [Uuid](uuid::Uuid)
+    ///   ```rust,ignore
+    ///   raw
+    ///   .as_slice()
+    ///   .chunks(16)
+    ///   .map(|x: &[u8]| uuid::Uuid::from_bytes(*<&[u8] as std::convert::TryInto<&[u8;16]>>::try_into(x).expect("invalid uuid slice")))
+    ///   .collect();
+    ///   ```
     fn into_dart(self) -> DartCObject {
         self.into_iter()
             .map(|x| x.as_bytes().to_vec())
